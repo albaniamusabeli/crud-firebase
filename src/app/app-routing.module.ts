c2288f9633793d9c60610562af6e3c2cf762d685
Albania Musabeli <wfav277@gmail.com>
2022-01-07 21:52:27 -0300

    Se da estilo y se agregan rutas
    Agregar fondo en gradiente
    Agregar iconos font-awesome
    Configurar rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'lista-empleados', pathMatch: 'full'},
+  {path: 'lista-empleados', component: ListaEmpleadosComponent},
+  {path: 'crear-empleado', component: CrearEmpleadoComponent},
+  {path: '**', redirectTo: 'lista-empleados', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
