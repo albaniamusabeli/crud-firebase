b5954a20e50b69631267c4bcc2295b4a28ad6da8
Albania Musabeli <wfav277@gmail.com>
2022-01-09 15:41:18 -0300

    Agregar  y configurar Firebase - Firestore Database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 import { AppComponent } from './app.component';
 import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
 import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    //provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'crud-empleados-bani',
+    appId: '1:409894329987:web:4e3f28b249e477847b05b6',
+    storageBucket: 'crud-empleados-bani.appspot.com',
+    apiKey: 'AIzaSyCGr7o9hUYkUmn-7Ktz3XObVt8czZPbZw0',
+    authDomain: 'crud-empleados-bani.firebaseapp.com',
+    messagingSenderId: '409894329987',
+  },
   production: true
 };
